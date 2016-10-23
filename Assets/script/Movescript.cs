7dadb9abfedb0a392074f84cdf7730a4f270f0ab
catamiaw <catamiaw.dev@gmail.com>
2016-10-24 00:54:35 +0200

    no message

---
+++ Assets/script/Movescript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movescript : MonoBehaviour
+{
+	public Vector2 speed = new Vector2 (100, 100);
+	public Vector2 direction = new Vector2(-1,0);
+
+	private Vector2 mouvement;
+
+	void Update()
+	{
+		mouvement = new Vector2 (speed.x * direction.x, speed.y * direction.y);
+	}
+
+	void FixedUpdate()
+	{
+		GetComponent<Rigidbody2D>().velocity = mouvement * Time.deltaTime;
+	}
+}
